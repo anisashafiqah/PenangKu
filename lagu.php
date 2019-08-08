d820afe2435bb292f4ec52759856c0c9bd608a78
anisashafiqah <53892674+anisashafiqah@users.noreply.github.com>
2019-08-08 15:46:55 +0800

    AnisaShafiqah

---
+++ lagu.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<br>
+<br>
+<br>
+<img src="Bendera Penang.png" width="310" height="180">
+<br>
+<br><br>
+<h2>
+Selamat Tuhan kurniakan,
+<br>
+Selamat Pulau Pinang,
+<br>
+Negeriku yang mulia,
+<br>
+Kutaat dan setia,
+<br>
+Aman dan bahagia,
+<br>
+Majulah jayalah,
+<br>
+Negeriku yang ku cinta,
+<br>
+Bersatu dan bersama,
+<br>
+Untuk negeri kita.
+</h2>
+</center>
+</body>
+</html>
---
+++ penanghills.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<br>
+<br>
+<br>
+<h1> Penang Hills </h1>
+<img src="Penang Hill 1.jpg" width="350" height="230">
+<img src="Penang Hill 2.png" width="350" height="230">
+<br>
+<br><br>
+<h3>
+Bukit Hill adalah stesen bukit kolonial pertama yang dibangunkan di Semenanjung Malaysia.
+<br> 
+Merangkumi Bukit Barat, Bukit Laksamana, Tiger Hill, Flagstaff Hill dan Government Hill, 
+<br>ia terletak kira-kira enam km dari Georgetown. 
+<br>
+Kawasan bukit dan berhutan adalah resort bukit utama negeri. 
+<br>
+Set 821m di atas ibu kota Pulau Pinang, penduduk pulau itu memanggilnya Bukit Bendera dan 
+<br>
+umumnya kira-kira lima darjah lebih sejuk daripada Georgetown. 
+Ia merupakan patch terakhir hutan hujan tropika di Pulau Pinang jadi flora dan fauna di 
+<br>
+sini telah dilindungi sejak tahun 1960.
+<br>
+Ia tidak mempunyai keunggulan yang sama seperti Genting Highlands, Fraser's Hill atau Cameron Highlands 
+<br>
+tetapi ia adalah salah satu tarikan pelancong yang terkenal 
+<br>
+di Pulau Pinang kepada iklim yang lebih segar.
+</h3>
+
+</body>
+</html>
